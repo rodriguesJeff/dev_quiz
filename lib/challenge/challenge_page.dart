aff35c109f88179b25ba73f30e651bfc518be5d2
Jefferson Rodrigues <jeffersondavid179@gmail.com>
2021-04-25 13:22:31 -0300

    Added buttons for navigation on challenge page

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:dev_quiz/challenge/widgets/quiz/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +18,19 @@ class _ChallengePageState extends State<ChallengePage> {
       ),
       body: QuizWidget(
         title: 'O que o Flutter faz em sua totalidade?',
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(child: NextButtonWidget.white(label: 'Pular', onTap: () {},)),
+              SizedBox(width: 7),
+              Expanded(child: NextButtonWidget.green(label: 'Confirmar', onTap: () {}))
+            ],
+          ),
+        ),
       ),
     );
   }
